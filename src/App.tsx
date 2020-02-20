8e5beddec34806f6192311209d523ee9798cd96e
Michael Cook <michael.cook@thetechdept.com>
2020-02-20 16:26:50 +0000

    API test, state input, debouncing. Good stuff!

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,49 @@ import React, { Component } from "react";
 import "./App.scss";
 
 export default class extends Component {
+  state = { phrase: "" };
+  apiDebouncer: number = 0;
+
   componentDidMount() {
-    let url = "https://api.datamuse.com/words?ml=hell";
+    let url = `https://iinvq9pu20.execute-api.us-east-1.amazonaws.com/dev/synonyms?phrase=hell+explorer`;
     fetch(url)
       .then(r => {
         return r.json();
       })
       .then(data => {
-        console.log(data);
+        const { synonyms } = data;
+        console.log(synonyms);
       });
   }
+  getSynonyms(phrase: string) {
+    console.log("phrase", phrase);
+  }
+  inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
+    clearTimeout(this.apiDebouncer);
+    this.apiDebouncer = window.setTimeout(() => {
+      this.getSynonyms(this.state.phrase);
+    }, 500);
+  };
   render() {
-    return <>Hey up..</>;
+    return (
+      <main>
+        <form
+          onSubmit={e => {
+            e && e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            name="phrase"
+            value={this.state.phrase}
+            placeholder="Cool phrase.."
+            onChange={this.inputHandler}
+            onInput={this.inputHandler}
+          />
+        </form>
+      </main>
+    );
   }
 }
